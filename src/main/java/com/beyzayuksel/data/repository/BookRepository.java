4e8f5735497f0b49b989cef755e3068296d15309
BEYZA <69187640+beyzayuksell@users.noreply.github.com>
2022-05-28 14:32:06 +0300

    book reapository added.

---
+++ src/main/java/com/beyzayuksel/data/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.beyzayuksel.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository {
+
+}
