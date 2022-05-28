f068467f6be03385d7ad23a476ca6c6529845c4e
BEYZA <69187640+beyzayuksell@users.noreply.github.com>
2022-05-28 22:48:28 +0300

    tests added and all test passed. test writed for crud process

--- src/test/java/com/beyzayuksel/FinalprojectTechApplicationTests.java
+++ src/test/java/com/beyzayuksel/FinalprojectTechApplicationTests.java
@@ -1,13 +1,74 @@
 package com.beyzayuksel;
 
+import com.beyzayuksel.data.entity.BookEntity;
+import com.beyzayuksel.data.repository.BookRepository;
+import com.beyzayuksel.iotest.ITestData;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinalprojectTechApplicationTests {
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FinalprojectTechApplicationTests implements ITestData {
+
+    // JUNIT Tests
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // SAVE
+    @Override
     @Test
-    void contextLoads() {
+    public void testCreate() {
+        BookEntity entity = new BookEntity();
+        entity.setBookTitle("Suç ve Ceza");
+        entity.setBookAuthor("Dostoyevski");
+        entity.setBookPublishedYear("1884");
+        bookRepository.save(entity);
+        // eger: 1 id'li data'yı bulamazsa Exception fırlat: java.util.NoSuchElementException: No value present
+        assertNotNull(bookRepository.findById(1L).get());
+
+    }
+    // FIND
+    @Override
+    @Test
+    public void testFind() {
+        BookEntity entity = bookRepository.findById(1L).get();
+        // Belirtilen id bulunmazsa: java.util.NoSuchElementException: No value present
+        assertEquals("1887", entity.getBookPublishedYear());
+    }
+    // LIST
+    @Override
+    @Test
+    public void testList() {
+        List<BookEntity> entityList=bookRepository.findAll();
+        // java.lang.AssertionError:  Expecting actual:0  to be greater than:
+        assertThat(entityList).size().isGreaterThan(0);
     }
 
+    //UPDATE
+    @Override
+    @Test
+    public void testUpdate() {
+        BookEntity entity = bookRepository.findById(1L).get();
+        entity.setBookTitle("Yakut Kırmızısı");
+        bookRepository.save(entity);
+
+        //org.opentest4j.AssertionFailedError: expected: not equal but was: <Yakut Kırmızısı>
+        //unexpected:Beklenen
+        assertNotEquals("Yakut Kırmızısı",bookRepository.findById(1L).get().getBookTitle());
+
+    }
+    // DELETE
+    @Override
+    @Test
+    public void testDelete() {
+        bookRepository.deleteById(1L);
+        // istenilen data yoksa: No class com.beyzayuksel.data.entity.BookEntity entity with id 1 exists!
+        assertThat(bookRepository.existsById(1L)).isFalse();
+    }
 }
---
+++ src/test/java/com/beyzayuksel/iotest/ITestData.java
@@ -0,0 +1,21 @@
+package com.beyzayuksel.iotest;
+
+public interface ITestData {
+
+    // Testlerde return tipi her zaman void olur.
+    
+    // SAVE
+    public void testCreate();
+
+    // FIND
+    public void testFind();
+
+    // LIST
+    public void testList();
+
+    // UPDATE
+    public void testUpdate();
+
+    // DELETE
+    public void testDelete();
+}
